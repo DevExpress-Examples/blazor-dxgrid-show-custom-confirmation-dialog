92bdddcbf34cd6500a4c22dde9868ae5bf1c8dcd
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-06-09 13:03:42 +0300

    upd

--- CS/DataGridShowDeleteConfirmation/Data/WeatherForecastService.cs
+++ CS/DataGridShowDeleteConfirmation/Data/WeatherForecastService.cs
@@ -1,22 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DataGridShowDeleteConfirmation.Data
-{
-    public class WeatherForecastService
-    {
+namespace DataGridShowDeleteConfirmation.Data {
+    public class WeatherForecastService {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public Task<List<WeatherForecast>> GetForecastAsync(DateTime startDate)
-        {
+        public Task<List<WeatherForecast>> GetForecastAsync(DateTime startDate) {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast {
                 ID = index,
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
---
+++ CS/DataGridShowDeleteConfirmation/DataGridShowDeleteConfirmation/Data/WeatherForecastService.cs
@@ -0,0 +1,18 @@
+namespace DataGridShowDeleteConfirmation.Data {
+    public class WeatherForecastService {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<List<WeatherForecast>> GetForecastAsync(DateTime startDate) {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+                ID = index,
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToList());
+        }
+    }
+}
