350a5374ae7b2298e4a0ed6b3f86d114a0209da2
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2022-07-27 21:54:07 -0800

    replace solution

--- CS/DataGridShowDeleteConfirmation/Data/WeatherForecast.cs
+++ CS/DataGridShowDeleteConfirmation/Data/WeatherForecast.cs
@@ -1,9 +1,15 @@
-namespace DataGridShowDeleteConfirmation.Data {
-    public class WeatherForecast {
-        public int ID { get; set; }
+using System;
+
+namespace DataGridShowDeleteConfirmation.Data
+{
+    public class WeatherForecast
+    {
         public DateTime Date { get; set; }
+
         public int TemperatureC { get; set; }
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-        public string? Summary { get; set; }
+
+        public string Summary { get; set; }
     }
 }
